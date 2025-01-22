25e36597ed6fefbc1ecb991add647baf79f0e4d5
Anupam <9582anupamk@gmail.com>
2025-01-23 01:06:00 +0530

    Add FeaturedCourses component and update Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "@/components/Hero";
-
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
     return (
         <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
             <Hero />
+            <FeaturedCourses />
         </main>
     );
 }
