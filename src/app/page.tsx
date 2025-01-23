816dda84b285ae531ed10aaa0c2e43c3591be2d9
Anupam <9582anupamk@gmail.com>
2025-01-23 12:10:01 +0530

    Add UpcomingWebinars component and HoverEffect UI, update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Testimonials from "@/components/Testimonials";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <FeaturedCourses />
             <WhyChooseUs />
             <Testimonials />
+            <UpcomingWebinars />    
         </main>
     );
 }
