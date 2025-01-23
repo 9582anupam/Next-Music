46652700eca7d0107695ef0471ca8654c4ad37ce
Anupam <9582anupamk@gmail.com>
2025-01-23 12:24:18 +0530

    Add Footer component and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import Testimonials from "@/components/Testimonials";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
@@ -14,6 +15,7 @@ export default function Home() {
             <Testimonials />
             <UpcomingWebinars />
             <Instructors />
+            <Footer />
         </main>
     );
 }
